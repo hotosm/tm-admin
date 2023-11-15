acde6cb1e0ba819a76fe173684ad943f5f2fc9ea
Rob Savoye <rob@senecass.com>
2023-11-15 10:15:39 -0700

    fix: Move protobuf creation code to it's own file and use it

--- tm_admin/tmadmin-manage.py
+++ tm_admin/tmadmin-manage.py
@@ -25,6 +25,7 @@ import sys
 from pathlib import Path
 from sys import argv
 from osm_rawdata.postgres import uriParser, PostgresClient
+from tm_admin.proto import ProtoBuf
 
 
 # Instantiate logger
@@ -113,159 +114,7 @@ class TmAdminManage(object):
         with open(sqlfile, 'r') as file:
             sql = file.read()
             return self.pg.createTable(sql)
-
-    def createProto(self,
-                    sqlfile: str,
-                    ):
-        """
-        Read an SQL file and produce data to create protobuf .proto files.
-
-        Args:
-            sqlfile (str): The SQL file to process.
-
-        Returns:
-            (dict): The list of enums and types
-            (dict): The list of tables
-        """
-        tables = dict()
-        enums = dict()
-        table = dict()
-        log.info(f"Processing {sqlfile}...")
-        with open(sqlfile, 'r') as file:
-            inblock = False
-            inenum = True
-            nume = list()
-            name = None
-            for line in file.readlines():
-                optional = ""
-                repeated = ""
-                # We don't care about the NOT NULL parameter
-                loc = line.find('NOT')
-                tmp = ' '.join(line[:loc].split()).split()
-                # optional is for anything that is NOT NULL
-                if loc > 0:
-                    optional = "optional "
-                if line[:2] == ");":
-                    if inblock:
-                        inblock = False
-                        tables[name] = table
-                        print(f"Done table {name}...")
-                    elif inenum:
-                        inenum = False
-                        enums[name] = nume
-                        print(f"Done enum {name}...")
-                    continue
-                if line[:12] == 'CREATE TABLE':
-                    name = tmp[2].replace("public.", "")
-                    inblock = True
-                    continue
-                if line[:11] == 'CREATE TYPE' and tmp[4] == 'ENUM':
-                    name = tmp[2].replace("public.", "")
-                    nume = list()
-                    inenum = True
-                    continue
-                if len(tmp) == 2 and inblock:
-                    table[tmp[0]] = tmp[1]
-                    continue
-                if len(tmp) == 3 and inblock:
-                    if tmp[2][-1:] == ',':
-                        table[tmp[0]] = f"{tmp[1]}_{tmp[2][:-1]}"
-                    else:
-                        table[tmp[0]] = f"{tmp[1]}_{tmp[2]}"
-                    continue
-                if len(tmp) == 1 and inenum:
-                    if  tmp[0][-1:] == ',':
-                        nume.append(f"{tmp[0][1:-2]}")
-                    else:
-                        nume.append(f"{tmp[0][1:-1]}")
-                # Repeated is for arrays
-                if line[:2] == "[]":
-                    repeated = "repeated"
-
-        out1 = self.createEnumProto(enums)
-        out2 = self.createTableProto(tables)
-
-        return out1, out2
-
-    def createEnumProto(self,
-                    enums: dict,
-                    ):
-        """
-        Process a list of enums into the protobuf version.
-
-        Args:
-            enums (dict): The list of tables to generate a protobuf for.
-
-        Returns:
-            (list): The list of enums in protobuf format
-        """
-        out = list()
-        out.append(f"syntax = 'proto3';")
-        for name, value in enums.items():
-            index = 0
-            out.append(f"enum {name.capitalize()} {{")
-            for entry in value:
-                out.append(f"\t{entry} = {index};")
-                index += 1
-            out.append('};')
-
-        return out
-
-    def createTableProto(self,
-                    tables: dict,
-                    ):
-        """
-        Process a list of tables into the protobuf version.
-
-        Args:
-            table (dict): The list of tables to generate a protobuf for.
-
-        Returns:
-            (list): The list of tables in protobuf format
-        """
-        out = list()
-        convert = {'integer': 'int32',
-                   'bigint': 'int64',
-                   'boolean': 'bool',
-                   'character_varying': 'string',
-                   'bytea': 'bytes',
-                   }
-        out.append(f"syntax = 'proto3';")
-        out.append("import 'types.proto';")
-
-        for name, value in tables.items():
-            out.append(f"message {name} {{")
-            index = 1
-            for table, data in tables.items():
-                optional = ""
-                repeated = ""
-                for k1, v1 in data.items():
-                    # Remove any embedded commas
-                    v1 = v1.replace(',', '')
-                    # print(f"DATA: {k1} = {v1}")
-                    if v1[:7] == 'public.':
-                        # log.debug(f"Got enum '{k1}' from types.proto")
-                        v1 = v1[7:].capitalize()
-                        # out.append(f"\t{optional}  {v1} {k1} = {index};")
-                    # FIXME: Sometimes this has a number for a fixed length string
-                    elif v1[:17] == 'character_varying':
-                        v1 = v1[:17]
-                    # It's an array, ie.. repeated field
-                    elif v1[-2:] == '[]':
-                        # log.debug(f"Got an array for {k1}")
-                        v1 = v1[:-2]
-                        repeated = 'repeated'
-                    # it's an enum defined in types.proto
-                    if v1 not in convert:
-                        if v1[:8] == 'Geometry':
-                            # log.debug(f"Got a geometry for {k1}")
-                            out.append(f"\t{optional}  bytes {k1} = {index};")
-                    else:
-                        out.append(f"\t{optional} {repeated} {convert[v1]} {k1} = {index};")
-                    index += 1
-            out += f"}}";
-
-        return out
+        return sql
 
     def readDiff(self,
                  diff: str,
@@ -313,14 +162,13 @@ def main():
     )
     choices = ['create', 'migrate']
     parser.add_argument("-v", "--verbose", nargs="?", const="0", help="verbose output")
-    parser.add_argument("-i", "--infile", default='.', help="Input SQL file")
     parser.add_argument("-d", "--diff", help="SQL file diff for migrations")
     parser.add_argument("-p", "--proto", help="Generate the .proto file from the SQL file")
     parser.add_argument("-u", "--uri", default='localhost/tm_admin',
                             help="Database URI")
     parser.add_argument("-c", "--cmd", choices=choices, default='create',
                             help="Command")
-    args = parser.parse_args()
+    args, sqlfiles = parser.parse_known_args()
 
     if len(argv) <= 1:
         parser.print_help()
@@ -335,27 +183,25 @@ def main():
         ch.setFormatter(formatter)
         log.addHandler(ch)
 
-    sqlfiles = list()
-    path = Path(args.infile)
-    if path.is_dir():
-        sqlfiles = path.glob('*/*.sql')
-    else:
-        sqlfiles = [path]
-
+    # The base class that does all the work
     tm = TmAdminManage(args.uri)
+
+    # modules to generatihng proto files
+    pb = ProtoBuf()
 
     # The default is the current directory in the source tree, so find all
     # the SQL files.
     result = tm.createTable("schemas.sql")
-    types = tm.createProto("types.sql")
+    # This defines all the enums needed to compile the proto files
+    types = pb.createProto("types.sql")
     with open('types.proto', 'w') as file:
         file.writelines(str(i)+'\n' for i in types[0])
         file.close()
 
-    for table in sqlfiles:
-        # table = tm.createTable(table)
-        out1, out2 = tm.createProto(table)
-        name = f"{table.parts[0]}/{table.stem}.proto"
+    for sqlfile in sqlfiles:
+        sql = tm.createTable(sqlfile)
+        out1, out2 = pb.createProto(sqlfile)
+        name = sqlfile.replace('.sql', '.proto')
         if len(out1) > 0:
             with open(name, 'w') as file:
                 file.writelines([str(i)+'\n' for i in out1])
@@ -366,7 +212,6 @@ def main():
                 file.close()
         log.info(f"Wrote {name} to disk")
     tm.dump()
-
 
 if __name__ == "__main__":
     """This is just a hook so this file can be run standalone during development."""
