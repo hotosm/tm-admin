e181f616f1420221e16744cadc3fb87cc0619b0e
Rob Savoye <rob.savoye@hotosm.org>
2023-12-11 16:01:35 -0700

    fix: Use DBSupport as a base class

--- tm_admin/users/users.py
+++ tm_admin/users/users.py
@@ -35,75 +35,16 @@ from osm_rawdata.postgres import uriParser, PostgresClient
 # Instantiate logger
 log = logging.getLogger(__name__)
 
-class UsersDB(object):
+class UsersDB(DBSupport):
     def __init__(self,
                  dburi: str = "localhost/tm_admin",
                 ):
         self.pg = None
         self.profile = UsersTable()
-        if dburi:
-            self.pg = PostgresClient(dburi)
+        #if dburi:
+        #    self.pg = PostgresClient(dburi)
         self.types = dir(tm_admin.types_tm)
-        self.queries = DBSupport('users', dburi)
-
-    def createUser(self,
-                    profile: UsersTable,
-                    ):
-        self.profile = profile
-        self.queries.createTable(profile)
-
-    def updateUser(self,
-                    profile: UsersTable,
-                    id: int = None,
-                    ):
-        self.profile = profile
-        sql = f"UPDATE users SET"
-        if not id:
-            id = profile.data['id']
-        for column,value in self.profile.data.items():
-            name = column.replace('_', '').capitalize()
-            if name in self.types:
-                # FIXME: this needs to not be hardcoded!
-                tmp = tm_admin.types_tm.Mappinglevel._member_names_
-                if type(value) == str:
-                    level = value
-                else:
-                    level = tmp[value-1]
-                sql += f" {column}='{level}'"
-                continue
-            if value:
-                try:
-                    # FIXME: for now ignore timestamps, as they're meaningless
-                    # between projects
-                    if parse(value):
-                        continue
-                except:
-                    # it's a string, but not a date
-                    pass
-                sql += f" {column}='{value}',"
-        sql += f" WHERE id='{id}'"
-        # print(sql)
-        result = self.pg.dbcursor.execute(f"{sql[:-1]}';")
-
-    def resetSequence(self):
-        sql = "ALTER SEQUENCE public.users_id_seq RESTART;"
-        self.pg.dbcursor.execute(sql)
-
-    def getByID(self,
-                id: int,
-                ):
-        data = self.queries.getByID(id)
-        return data
-
-    def getByName(self,
-                name: str,
-                ):
-        data = self.queries.getByName(name)
-        return data
-
-    def getAllUsers(self):
-        data = self.queries.getAll()
-        return data
+        super().__init__('users', dburi)
 
 def main():
     """This main function lets this class be run standalone by a bash script."""
@@ -132,11 +73,11 @@ def main():
 
     user = UsersDB(args.uri)
     # user.resetSequence()
-    all = user.getAllUsers()
+    all = user.getAll()
     # Don't pass id, let postgres auto increment
     ut = UsersTable(name='test', mapping_level='BEGINNER',
                     email_address='foo@bar.com')
-    user.createUser(ut)
+    user.createTable(ut)
     # print(all)
 
     all = user.getByID(1)
@@ -147,7 +88,7 @@ def main():
 
     ut = UsersTable(name='foobar', email_address="bar@foo.com", mapping_level='INTERMEDIATE')
     # This is obviously only for manjal testing
-    user.updateUser(ut, 17)
+    #user.updateTable(ut, 17)
 
 if __name__ == "__main__":
     """This is just a hook so this file can be run standalone during development."""
