8924a371352f9d9b726e9d8d82a5a0f2045d9d37
Rob Savoye <rob@senecass.com>
2023-12-19 11:51:03 -0700

    fix: Ad dminimal testcase for YAML config file parsing

---
+++ tests/test_yamlfile.py
@@ -0,0 +1,75 @@
+#!/usr/bin/python3
+
+# Copyright (c) 2022, 2023 Humanitarian OpenStreetMap Team
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+# Humanitarian OpenStreetmap Team
+# 1100 13th Street NW Suite 800 Washington, D.C. 20005
+# <info@hotosm.org>
+
+import argparse
+import logging
+import sys
+import os
+from sys import argv
+from tm_admin.yamlfile import YamlFile
+from datetime import datetime
+
+# Instantiate logger
+log = logging.getLogger(__name__)
+
+import tm_admin as tma
+rootdir = tma.__path__[0]
+
+config = f'{rootdir}/projects/projects.yaml'
+
+def test_all():
+    yaml = YamlFile(config)
+    # yaml.dump()
+    data = yaml.getEntries()
+    hits = 0
+    if data['id']['datatype'] == 'int64':
+        hits += 1
+
+    if data['difficulty']['datatype'] == 'Projectdifficulty':
+        hits += 1
+
+    if data['mapper_level']['share'] == True:
+        hits += 1
+
+    assert hits == 3
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-v", "--verbose", nargs="?", const="0", help="verbose output")
+    parser.add_argument("-i", "--infile", default=f'{config}', help="YAML config file")
+    args = parser.parse_args()
+
+    # if verbose, dump to the terminal.
+    log_level = os.getenv("LOG_LEVEL", default="INFO")
+    if args.verbose is not None:
+        log_level = logging.DEBUG
+
+    logging.basicConfig(
+        level=log_level,
+        format=("%(asctime)s.%(msecs)03d [%(levelname)s] " "%(name)s | %(funcName)s:%(lineno)d | %(message)s"),
+        datefmt="%y-%m-%d %H:%M:%S",
+        stream=sys.stdout,
+    )
+
+    print("--- test_all() ---")
+    test_all()
+    
+    
