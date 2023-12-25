b60e0e21083bc75d77159d1919817d7193e7af90
Rob Savoye <rob@senecass.com>
2023-12-25 14:31:09 -0700

    fix: Add support to merge the TM project_info data into the TM Admin projects table

--- tm_admin/projects/projects.py
+++ tm_admin/projects/projects.py
@@ -28,18 +28,44 @@ from datetime import datetime
 from dateutil.parser import parse
 import tm_admin.types_tm
 import geojson
+import concurrent.futures
+from cpuinfo import get_cpu_info
 from shapely.geometry import shape
 from shapely import centroid
 from tm_admin.types_tm import Mappingtypes, Projectstatus, Taskcreationmode, Editors, Permissions, Projectpriority, Projectdifficulty
+from tm_admin.projects.projects_class import ProjectsTable
 from shapely import wkb, get_coordinates
 from tm_admin.dbsupport import DBSupport
 from tm_admin.generator import Generator
 from osm_rawdata.postgres import uriParser, PostgresClient
 import re
 
+# The number of threads is based on the CPU cores
+info = get_cpu_info()
+cores = info["count"]
+
 # Instantiate logger
 log = logging.getLogger(__name__)
 
+def importThread(
+    data: list,
+    db: PostgresClient,
+):
+    """Thread to handle importing
+
+    Args:
+        data (list): The list of records to import
+        db (PostgresClient): A database connection
+    """
+    for record in data:
+        sql = f" UPDATE projects SET licenses = ARRAY[{record[0]['license']}] WHERE id={record[0]['user']}"
+        print(sql)
+        # try:
+        #     result = db.dbcursor.execute(f"{sql};")
+        # except:
+        #     return False
+
+    return True
 
 class ProjectsDB(DBSupport):
     def __init__(self,
@@ -59,14 +85,55 @@ class ProjectsDB(DBSupport):
         self.types = dir(tm_admin.types_tm)
         super().__init__('projects', dburi)
 
+    def mergeProjectInfo(self,
+                         inuri: str,
+                         ):
+        table = 'project_info'
+        pg = PostgresClient(inuri)
+        sql = f"SELECT row_to_json({table}) as row FROM {table}"
+        # print(sql)
+        try:
+            result = pg.dbcursor.execute(sql)
+        except:
+            log.error(f"Couldn't execute query! {sql}")
+            return False
+
+        ignore = ['project_id', 'project_id_str', 'text_searchable', 'per_task_instructions']
+        # Extract the list of columns
+        id = 0
+        for record in pg.dbcursor.fetchall():
+            settings = ""
+            column = list()
+            for key, val in record[0].items():
+                if key in ignore:
+                    continue
+                if key == "project_id":
+                    key = "id"
+                elif key == "locale":
+                    key = "default_locale"
+                else:
+                    if val is None:
+                        continue
+                    key = f"{key}"
+                if type(val) == int:
+                    entry = f"{key}={val}"
+                else:
+                    entry = f"{key}='{val.replace("'", "&apos;")}'"
+                settings += f"{entry}, "
+
+            sql = f"UPDATE projects SET {settings[:-2]} WHERE id={record[0]['project_id']};"
+            print(sql)
+            results = self.pg.queryLocal(sql[:-2])
+
 def main():
     """This main function lets this class be run standalone by a bash script."""
     parser = argparse.ArgumentParser()
     parser.add_argument("-v", "--verbose", nargs="?", const="0", help="verbose output")
-    parser.add_argument("-u", "--uri", default='localhost/tm_admin',
+    parser.add_argument("-i", "--inuri", default='localhost/tm4',
                             help="Database URI")
-    parser.add_argument("-b", "--boundary", required=True,
-                        help="The project AOI")
+    parser.add_argument("-o", "--outuri", default='localhost/tm_admin',
+                            help="Database URI")
+    parser.add_argument("-b", "--boundary", help="The project AOI")
 
     # parser.add_argument("-r", "--reset", help="Reset Sequences")
     args = parser.parse_args()
@@ -87,20 +154,23 @@ def main():
         stream=sys.stdout,
     )
 
-    proj = ProjectsDB(args.uri)
+    proj = ProjectsDB(args.outuri)
     # user.resetSequence()
     all = proj.getAll()
 
-    file = open(args.boundary, 'r')
-    boundary = geojson.load(file)
-    geom = shape(boundary[0]['geometry'])
-    center = centroid(geom)
+    proj.mergeProjectInfo(args.inuri)
+
+    # file = open(args.boundary, 'r')
+    # boundary = geojson.load(file)
+    # geom = shape(boundary[0]['geometry'])
+    # center = centroid(geom)
+
     # Don't pass id, let postgres auto increment
-    ut = ProjectsTable(author_id=1, outline=geom, centroid=center,
-                       created='2021-12-15 09:58:02.672236',
-                       task_creation_mode='GRID', status='DRAFT',
-                       mapper_level='BEGINNER')
-    proj.createTable(ut)
+    # ut = ProjectsTable(author_id=1, outline=geom, centroid=center,
+    #                    created='2021-12-15 09:58:02.672236',
+    #                    task_creation_mode='GRID', status='DRAFT',
+    #                    mapping_level='BEGINNER')
+    # proj.createTable(ut)
     # print(all)
 
     #all = proj.getByID(1)
