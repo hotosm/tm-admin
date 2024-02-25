1131e14ec31b8526b140d57d818ef55130d42499
Rob Savoye <rob@senecass.com>
2024-02-25 15:28:12 -0700

    fix: Refactor to be async

--- tests/test_messages.py
+++ tests/test_messages.py
@@ -32,6 +32,8 @@ from datetime import datetime
 from tm_admin.users.users_class import UsersTable
 from tm_admin.messages.messages import MessagesDB
 from tm_admin.messages.messages_class import MessagesTable
+import asyncio
+from codetiming import Timer
 
 # Instantiate logger
 log = logging.getLogger(__name__)
@@ -42,154 +44,153 @@ rootdir = tma.__path__[0]
 # FIXME: For now these tests assume you have a local postgres installed. One has the TM
 # database, the other for tm_admin.
 
-user = None
-msg = None
+user = UsersDB()
+msg = MessagesDB()
 
-
-def get_all_messages():
+async def get_all_messages():
     log.debug(f"--- get_all_messages() unimplemented!")
     # user_id: int,
-    result = msg.getByWhere()
-    assert len(result) > 0
+    # result = await msg.getByWhere()
+    # assert len(result) > 0
 
-def get_message():
+async def get_message():
     """Gets the specified message to a user"""
     log.debug(f"--- get_message() ---")
     # message_id: int, to_ser_id: int) -> Message:
     mid = 599781
     uid = 8576693
-    result = msg.getByWhere(f" id={mid} AND to_user_id={uid}")
-    assert len(result) > 0
+    # result = await msg.getByWhere(f" id={mid} AND to_user_id={uid}")
+    # assert len(result) > 0
 
-def send_welcome_message():
+async def send_welcome_message():
     """Sends welcome message to new user at Sign up"""
     # user: User):
     log.debug(f"--- send_welcome_message() unimplemented!")
 
-def send_message_after_validation():
+async def send_message_after_validation():
     # ):
     log.debug(f"--- send_message_after_validation() unimplemented!")
     # status: int, validated_by: int, mapped_by: int, task_id: int, project_id: int
 
-def send_message_to_all_contributors():
+async def send_message_to_all_contributors():
     # project_id: int, message_dto: MessageDTO):
     log.debug(f"--- send_message_to_all_contributors() unimplemented!")
     """Sends supplied message to all contributors on specified project."""
 
-def _push_messages():
+async def _push_messages():
     # messages):
     log.debug(f"--- _push_messages() unimplemented!")
 
-def send_message_after_comment():
+async def send_message_after_comment():
     log.debug(f"--- send_message_after_comment() unimplemented!")
     # comment_from: int, comment: str, task_id: int, project_id: int
 
-def send_project_transfer_message():
+async def send_project_transfer_message():
     log.debug(f"--- send_project_transfer_message() unimplemented!")
     # project_id: int,
 
-def get_user_link():
+async def get_user_link():
     # username: str):
     log.debug(f"--- get_user_link() unimplemented!")
 
-def get_team_link():
+async def get_team_link():
     # team_name: str, team_id: int, management: bool):
     log.debug(f"--- get_team_link() unimplemented!")
 
-def send_request_to_join_team():
+async def send_request_to_join_team():
     log.debug(f"--- send_request_to_join_team() unimplemented!")
     # from_user: int, from_username: str, to_user: int, team_name: str, team_id: int
 
-def accept_reject_request_to_join_team():
+async def accept_reject_request_to_join_team():
     log.debug(f"--- accept_reject_request_to_join_team() unimplemented!")
     # from_user: int,
         
-def accept_reject_invitation_request_for_team():
+async def accept_reject_invitation_request_for_team():
     log.debug(f"--- accept_reject_invitation_request_for_team() unimplemented!")
     # from_user: int,
     
-def send_team_join_notification():
+async def send_team_join_notification():
     log.debug(f"--- send_team_join_notification() unimplemented!")
     # from_user: int,
 
-def send_message_after_chat():
+async def send_message_after_chat():
     log.debug(f"--- send_message_after_chat() unimplemented!")
     #    chat_from: int, chat: str, project_id: int, project_name: str
     
-def send_favorite_project_activities():
+async def send_favorite_project_activities():
     # user_id: int):
     log.debug(f"--- send_favorite_project_activities() unimplemented!")
 
-def resend_email_validation():
+async def resend_email_validation():
     """Resends the email validation email to the logged in user"""
     # user_id: int):
     log.debug(f"--- resend_email_validation() unimplemented!")
 
-def _parse_message_for_bulk_mentions():
+async def _parse_message_for_bulk_mentions():
     log.debug(f"--- _parse_message_for_bulk_mentions() unimplemented!")
     # message: str, project_id: int, task_id: int = None
     
-def _parse_message_for_username():
+async def _parse_message_for_username():
     log.debug(f"--- _parse_message_for_username() unimplemented!")
     # message: str, project_id: int, task_id: int = None
 
-def has_user_new_messages():
+async def has_user_new_messages():
     """Determines if the user has any unread messages"""
     # user_id: int) -> dict:
     log.debug(f"--- has_user_new_messages() unimplemented!")
-def mark_all_messages_read():
+async def mark_all_messages_read():
     """Marks all messages as read for the user"""
     # user_id: int, message_type: str = None):
     log.debug(f"--- mark_all_messages_read() unimplemented!")
 
-def mark_multiple_messages_read():
+async def mark_multiple_messages_read():
     """Marks the specified messages as read for the user"""
     # message_ids: list, user_id: int):
     log.debug(f"--- mark_multiple_messages_read() unimplemented!")
 
-def get_message_as_dto():
+async def get_message_as_dto():
     """Gets the selected message and marks it as read"""
     # message_id: int, user_id: int):
     log.debug(f"--- get_message_as_dto() unimplemented!")
 
-def delete_message():
+async def delete_message():
     """Deletes the specified message"""
     # message_id: int, user_id: int):
     log.debug(f"--- delete_message() unimplemented!")
 
-def delete_multiple_messages():
+async def delete_multiple_messages():
     """Deletes the specified messages to the user"""
     # message_ids: list, user_id: int):
     log.debug(f"--- delete_multiple_messages() unimplemented!")
 
-def delete_all_messages():
+async def delete_all_messages():
     """Deletes all messages to the user"""
     # user_id: int, message_type: str = None):
     log.debug(f"--- delete_all_messages() unimplemented!")
 
-def get_task_link():
+async def get_task_link():
     log.debug(f"--- get_task_link() unimplemented!")
     #project_id: int, task_id: int, base_url=None, highlight=False
 
-def get_project_link():
+async def get_project_link():
     log.debug(f"--- get_project_link() unimplemented!")
     # project_id: int,
 
-def get_user_profile_link():
+async def get_user_profile_link():
     """Helper method to generate a link to a user profile"""
     # user_name: str, base_url=None) -> str:
     log.debug(f"--- get_user_profile_link() unimplemented!")
 
-def get_user_settings_link():
+async def get_user_settings_link():
     """Helper method to generate a link to a user profile"""
     # section=None, base_url=None) -> str:
     log.debug(f"--- get_user_settings_link() unimplemented!")
 
-def get_organisation_link():
+async def get_organisation_link():
     log.debug(f"--- get_organisation_link() unimplemented!")
     # ganisation_id: int, organisation_name: str, base_url=None
 
-if __name__ == "__main__":
+async def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("-v", "--verbose", nargs="?", const="0", help="verbose output")
     parser.add_argument("-u", "--uri", default='localhost/tm_admin', help="Database URI")
@@ -207,37 +208,43 @@ if __name__ == "__main__":
         stream=sys.stdout,
     )
 
-    user = UsersDB(args.uri)
-    msg = MessagesDB(args.uri)
+    user.connect(args.uri)
+    msg.connect(args.uri)
 
-    send_welcome_message()
-    send_message_after_validation()
-    send_message_to_all_contributors()
-    _push_messages()
-    send_message_after_comment()
-    send_project_transfer_message()
-    get_user_link()
-    get_team_link()
-    send_request_to_join_team()
-    accept_reject_request_to_join_team()
-    accept_reject_invitation_request_for_team()
-    send_team_join_notification()
-    send_message_after_chat()
-    send_favorite_project_activities()
-    resend_email_validation()
-    _parse_message_for_bulk_mentions()
-    _parse_message_for_username()
-    has_user_new_messages()
-    get_all_messages()
-    get_message()
-    mark_all_messages_read()
-    mark_multiple_messages_read()
-    get_message_as_dto()
-    delete_message()
-    delete_multiple_messages()
-    delete_all_messages()
-    get_task_link()
-    get_project_link()
-    get_user_profile_link()
-    get_user_settings_link()
-    get_organisation_link()
+    await send_welcome_message()
+    await send_message_after_validation()
+    await send_message_to_all_contributors()
+    await _push_messages()
+    await send_message_after_comment()
+    await send_project_transfer_message()
+    await get_user_link()
+    await get_team_link()
+    await send_request_to_join_team()
+    await accept_reject_request_to_join_team()
+    await accept_reject_invitation_request_for_team()
+    await send_team_join_notification()
+    await send_message_after_chat()
+    await send_favorite_project_activities()
+    await resend_email_validation()
+    await _parse_message_for_bulk_mentions()
+    await _parse_message_for_username()
+    await has_user_new_messages()
+    await get_all_messages()
+    await get_message()
+    await mark_all_messages_read()
+    await mark_multiple_messages_read()
+    await get_message_as_dto()
+    await delete_message()
+    await delete_multiple_messages()
+    await delete_all_messages()
+    await get_task_link()
+    await get_project_link()
+    await get_user_profile_link()
+    await get_user_settings_link()
+    await get_organisation_link()
+
+if __name__ == "__main__":
+    """This is just a hook so this file can be run standalone during development."""
+    loop = asyncio.new_event_loop()
+    asyncio.set_event_loop(loop)
+    loop.run_until_complete(main())
